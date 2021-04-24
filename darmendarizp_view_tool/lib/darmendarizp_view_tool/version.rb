186534129dc39c9abffa748bd0c99b9bbe92eb5e
David Armendariz <darmendariz1998@outlook.com>
2021-04-23 21:28:19 -0500

    Initial commit

---
+++ darmendarizp_view_tool/lib/darmendarizp_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DarmendarizpViewTool
+  VERSION = "0.1.0"
+end
---
+++ darmendarizp_view_tool/spec/darmendarizp_view_tool_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DarmendarizpViewTool do
+  it "has a version number" do
+    expect(DarmendarizpViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
